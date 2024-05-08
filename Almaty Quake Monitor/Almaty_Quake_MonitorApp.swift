95d166e574a5380208cd0013559c5d18e11586a5
yergalibereket <b_yergali@outlook.com>
2024-05-08 20:18:22 +0500

    Initial Commit

---
+++ Almaty Quake Monitor/Almaty_Quake_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Almaty_Quake_MonitorApp.swift
+//  Almaty Quake Monitor
+//
+//  Created by Yergali Bereket on 08.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Almaty_Quake_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
